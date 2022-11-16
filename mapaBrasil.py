5d14209d2de02e994c062d4f7ea34a9bcda60b9c
Matheus Costa <72279998+mc-gomes@users.noreply.github.com>
2022-11-16 15:38:02 -0300

    refatoração do código

--- mapaBrasil.py
+++ mapaBrasil.py
@@ -1,69 +1,71 @@
-from collections import defaultdict
-
-
 class Grafo(object):
     """ Implementação básica de um grafo. """
 
-    def __init__(self, arestas, direcionado=False):
-        """Inicializa as estruturas base do grafo."""
-        self.adj = defaultdict(set)
-        self.direcionado = direcionado
-        self.adiciona_arestas(arestas)
+    adjacencias = {
+        "AC": ["AM", "RO"],
+        "AM": ["RO", "MT", "PA", "RR", "AC"],
+        "RO": ["AM", "MT", "AC"],
+        "RR": ["AM", "PA"],
+        "PA": ["TO", "MT", "AP", "AM", "MA", "RR"],
+        "AP": ["PA"],
+        "TO": ["PI", "MT", "BA", "GO", "PA", "MA"],
+        "MA": ["TO", "PA", "PI"],
+        "PI": ["TO", "PE", "BA", "MA", "CE"],
+        "BA": ["PI", "TO", "PE", "MG", "GO", "ES", "SE", "AL"],
+        "CE": ["PE", "PI", "PB", "RN"],
+        "RN": ["CE", "PB"],
+        "PB": ["CE", "RN", "PE"],
+        "PE": ["PI", "BA", "PB", "AL", "CE"],
+        "AL": ["PE", "BA", "SE"],
+        "SE": ["AL", "BA"],
+        "MT": ["TO", "RO", "GO", "PA", "AM", "MS"],
+        "GO": ["TO", "MG", "MT", "BA", "DF", "MS"],
+        "MS": ["SP", "PR", "MG", "MT", "GO"],
+        "DF": ["MG", "GO"],
+        "MG": ["SP", "RJ", "BA", "GO", "ES", "DF", "MS"],
+        "ES": ["MG", "RJ", "BA"],
+        "SP": ["MG", "RJ", "PR", "MS"],
+        "RJ": ["SP", "MG", "ES"],
+        "PR": ["SP", "SC", "MS"],
+        "SC": ["RS", "PR"],
+        "RS": ["SC"],
+    }
 
+    def __init__(self):
+        """Inicializa as estruturas base do grafo."""
+        self.lista_adj = self.adjacencias
 
     def get_vertices(self):
-        """ Retorna a lista de vértices do grafo. """
-        return list(self.adj.keys())
-
+        """ Retorna a lista dos nós do grafo. """
+        return list(self.lista_adj.keys())
 
     def get_arestas(self):
         """ Retorna a lista de arestas do grafo. """
-        return [(k, v) for k in self.adj.keys() for v in self.adj[k]]
-
-
-    def adiciona_arestas(self, arestas):
-        """ Adiciona arestas ao grafo. """
-        for u, v in arestas:
-            self.adiciona_arco(u, v)
-
-
-    def adiciona_arco(self, u, v):
-        """ Adiciona uma ligação (arco) entre os nodos 'u' e 'v'. """
-        self.adj[u].add(v)
-        # Se o grafo é não-direcionado, precisamos adicionar arcos nos dois sentidos.
-        if not self.direcionado:
-            self.adj[v].add(u)
-
+        return [(k, v) for k in self.lista_adj.keys() for v in self.lista_adj[k]]
 
     def existe_aresta(self, u, v):
-        """ Existe uma aresta entre os vértices 'u' e 'v'? """
-        return u in self.adj and v in self.adj[u]
+        """ Verifica se existe uma aresta entre os vértices 'u' e 'v' """
+        return u.upper() in self.lista_adj and v.upper() in self.lista_adj[u]
 
+    def total_de_nos(self):
+        """ Retorna o número total de nós no grafo."""
+        return len(self.lista_adj)
 
     def __len__(self):
-        return len(self.adj)
-
+        return len(self.lista_adj)
 
     def __str__(self):
-        return '{}({})'.format(self.__class__.__name__, dict(self.adj))
-
+        return '{}({})'.format(self.__class__.__name__, dict(self.lista_adj))
 
     def __getitem__(self, v):
-        return self.adj[v]
+        """ Retorna a lista de vizinhos de um nó.
 
-if __name__== "__main__":
-    # Cria a lista de arestas.
-    arestas = [('Acre', 'Amazonas'), ('Amazonas', 'Rondonia'), ('Amazonas', 'Roraima'), ('Amazonas', 'Mato Grosso'), ('Amazonas', 'Pará'),
-               ('Roraima', 'Pará'), ('Amapá', 'Pará'), ('Rondonia', 'Mato Grosso'), ('Mato Grosso', 'Pará'),('Mato Grosso', 'Tocantins'),
-               ('Mato Grosso', 'Goiás'),('Mato Grosso', 'Mato Grosso do Sul'),('Pará', 'Tocantins'), ('Pará', 'Maranhão'), ('Maranhão','Piauí'),
-               ('Maranhão','Tocantins'),('Maranhão','Piauí'), ('Tocantins','Bahia'),('Tocantins','Goiás'),('Maranhão','Piauí'), ('Ceará','Piauí'),
-               ('Ceará','Paraíba'), ('Ceará','Pernambuco'), ('Ceará','Rio Grande do Norte'), ('Rio Grande do Norte','Paraíba'), ('Paraíba','Pernambuco'),
-               ('Pernambuco','Alagoas'), ('Alagoas','Sergipe'), ('Sergipe','Bahia'), ('Pernambuco','Bahia'), ('Pernambuco','Alagoas'), ('Bahia','Alagoas'),
-               ('Bahia','Goiás'),('Bahia','Espirito Santo'), ('Goiás','Minas Gerais'), ('Goiás','Mato Grosso do Sul'), ('Bahia','Minas Gerais'),
-               ('Espirito Santo','Minas Gerais'),('Rio de Janeiro Santo','Minas Gerais'), ('Espirito Santo','Rio de Janeiro'), ('Minas Gerais','Mato Grosso do Sul'),
-               ('São Paulo','Mato Grosso do Sul'), ('Paraná','Mato Grosso do Sul'), ('São Paulo','Rio de Janeiro'), ('São Paulo','Paraná'), ('Santa Catarina','Paraná'),
-               ('Santa Catarina', 'Rio Grande do Sul'), ]
+        :param v: o nó que se deseja saber os vizinhos
+        """
+        return self.lista_adj[v.upper()]
 
+
+if __name__ == "__main__":
     # Cria e imprime o grafo.
-    grafo = Grafo(arestas, direcionado=False)
-    print(grafo.adj)
+    grafo = Grafo()
+    print(grafo.lista_adj)
